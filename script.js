a76fd2e857d518a8b31012a1dd07e1372a8194bc
Pete Fritz <pete@worksofwisdom.com>
2020-10-19 21:15:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Increments the delay on each item.
+$('.rolldown-list li').each(function () {
+  var delay = ($(this).index() / 6) + 's';
+  $(this).css({
+    webkitAnimationDelay: delay,
+    mozAnimationDelay: delay,
+    animationDelay: delay
+  });
+});
+
+$('#btnReload').click(function () {
+  $('#myList').removeClass('rolldown-list');
+  setTimeout(function () {
+    $('#myList').addClass('rolldown-list');
+  }, 1);
+});
+
